87ac10e5511630f205d7d7ca13f415cfe3f9c623
jason-nordheim <jason.nordheim@gmail.com>
2020-06-07 10:43:05 -0600

    setup main loop with draw and update function; setup return condition if it is not time to re-render the screen

--- game.js
+++ game.js
@@ -1,8 +1,27 @@
+let lastRenderTime = 0 
+const SNAKE_SPEED = 2 /* Number of times to move per second */
 
 /* Setup Game Loop */ 
 function main(currentTime) {
-    window.requestAnimationFrame(main) // asks the browser when I can render my next frame 
-    console.log(currentTime)
+    const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000
+    window.requestAnimationFrame(main) /* asks the browser when I can render my next frame */
+    
+    /* do not re-render if ...*/
+    if (secondsSinceLastRender < 1 / SNAKE_SPEED) return 
+    
+    lastRenderTime = currentTime
+    console.log(secondsSinceLastRender)
+    
+    update()
+    draw()
+}
+
+function update(){
+
+}
+
+function draw(){
+    
 }
 
 window.requestAnimationFrame(main)
